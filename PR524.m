8715b4164246e500fe9eaec8238f3ce14390faf6
Alex Yuan <aysz88@gmail.com>
2014-05-23 18:24:53 -0400

    Move most code into a PR524 class

---
+++ PR524.m
@@ -0,0 +1,84 @@
+classdef PR524 < serial
+    %PR524 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+        function meter = PR524()
+            port = FindPortOf('PR Instrument');
+            meter@serial(port);
+            
+            % Communication Settings:
+            % For some reason, the meter wants to be sent just one
+            % character at a time, so don't let MATLAB send CR/LF to the
+            % device.  Reading is fine though, since MATLAB will buffer the
+            % CR/LF.  The device doesn't seem to use any sort of standard
+            % flow control.
+            set(meter, 'Terminator', {'CR/LF', ''});
+            set(meter, 'FlowControl', 'none');
+            
+            fopen(meter);
+            send(meter, 'PHOTO'); % Set the meter to Remote Mode
+            
+            send(meter, 'C'); % Clear any errors
+            pause(0.1);
+            if get(meter, 'BytesAvailable') > 0
+                % error was cleared
+                % ignore any "Instrument error cleared" message
+                fscanf(meter);
+            end
+        end
+        
+        function delete(meter)
+            if meter.isvalid && strcmpi(get(meter, 'status'), 'open')
+                send(meter, 'Q'); % Turn off Remote Mode
+                fclose(meter);
+            end
+            delete@serial(meter)
+        end
+        
+        function send(meter, cmd)
+            % Must send one character at a time :(
+
+            % Make sure the command ends with CR/LF
+            if length(cmd) < 2 || ~strcmpi(cmd(end-1:end), sprintf('\r\n'))
+                cmd = sprintf('%s\r\n', strtrim(cmd));
+            end
+
+            %fprintf('Sending %s...\n', strtrim(cmd));
+
+            for idx = 1:length(cmd)
+                fprintf(meter, cmd(idx));
+            end
+        end
+
+        function varargout = sendAndRead(meter, cmd, format, pauseTime)
+            if nargin < 3
+                format = '%s'; % discards the CR/LF terminator
+            end
+            if nargin < 4
+                pauseTime = 0.1;
+                % TODO maybe: if strcmpi(cmd(1), 'M'); pauseTime = 2.0; end;
+                % or a "wait for data" flag that does the fscanf without checking for data?
+            end
+
+            varargout = cell(1, min(nargout, 1));
+
+            meter.send(cmd);
+            pause(pauseTime);
+
+            %fprintf('About to read %i bytes...', meter.BytesAvailable);
+            if get(meter, 'BytesAvailable') > 0
+                [varargout{:}] = fscanf(meter, format);
+            else
+                if nargout >= 1; varargout{1} = ''; end;
+                if nargout >= 2; varargout{2} = 0; end;
+                if nargout >= 3; varargout{3} = 'No response.'; end;
+                % TODO throw warning iff narargout < 3?
+            end
+        end
+    end
+end
+
--- Test_Photometer.m
+++ Test_Photometer.m
@@ -1,94 +1,19 @@
-function [  ] = Test_Photometer( port )
+function [  ] = Test_Photometer()
 %TEST_PHOTOMETER Communication test for the PR-524
 %   Creates a connection to the PR-524, and reads its serial number
 % By Alexander Yuan, April 2014
     caughtException = [];
-    
-    if nargin < 1 || isempty(port)
-        port = FindPortOf('PR Instrument');
-    end
 
     delete(instrfind()); % kludge in case of conflicts?
 
-    meter = open(port);
+    meter = PR524();
     try
-        sn = sendAndRead(meter, 'D110'); % read serial number
-        fprintf('%s\n', sn);
+        [~, sn] = sendAndRead(meter, 'D110', '%i,%i'); % read serial number
+        fprintf('Requested serial number: %i\n', sn);
     catch e
         caughtException = e;
     end
-    close(meter);
+    delete(meter);
 
     if ~isempty(caughtException); rethrow(caughtException); end;
-end
-
-function meter = open(port)
-    meter = serial(port);
-    
-    % Communication Settings:
-    % For some reason, the meter wants to be sent just one character at a
-    % time, so don't let MATLAB send CR/LF to the device.  Reading is fine
-    % though, since MATLAB will buffer the CR/LF.  The device doesn't seem
-    % to use any sort of standard flow control.
-    meter.Terminator = {'CR/LF', ''};
-    meter.FlowControl = 'none';
-    
-    fopen(meter);
-    send(meter, 'PHOTO'); % Set the meter to Remote Mode
-    
-    send(meter, 'C'); % Clear any errors
-    pause(0.1);
-    if meter.BytesAvailable > 0
-        % cleared an error; ignore any "Instrument error cleared" message
-        fscanf(meter);
-    end
-end
-
-function close(meter)
-    if meter.isvalid && strcmpi(meter.status, 'open')
-        send(meter, 'Q'); % Turn off Remote Mode
-        fclose(meter);
-    end
-    delete(meter);
-end
-
-function send(meter, cmd)
-    % Must send one character at a time :(
-    
-    % Make sure the command ends with CR/LF
-    if length(cmd) < 2 || ~strcmpi(cmd(end-1:end), sprintf('\r\n'))
-        cmd = sprintf('%s\r\n', strtrim(cmd));
-    end
-    
-    fprintf('Sending %s...\n', strtrim(cmd));
-    
-    for idx = 1:length(cmd)
-        fprintf(meter, cmd(idx));
-    end
-end
-
-function varargout = sendAndRead(meter, cmd, format, pauseTime)
-    if nargin < 3
-        format = '%s'; % discards the CR/LF terminator
-    end
-    if nargin < 4
-        pauseTime = 0.1;
-        % TODO maybe: if strcmpi(cmd(1), 'M'); pauseTime = 2.0; end;
-        % or a "wait for data" flag that does the fscanf without checking for data?
-    end
-    
-    varargout = cell(1, nargout);
-    
-    send(meter, cmd);
-    pause(pauseTime);
-    
-    fprintf('About to read %i bytes...', meter.BytesAvailable);
-    if meter.BytesAvailable > 0
-        [varargout{:}] = fscanf(meter, format);
-    else
-        if nargout >= 1; varargout{1} = ''; end;
-        if nargout >= 2; varargout{2} = 0; end;
-        if nargout >= 3; varargout{3} = 'No response.'; end;
-        % TODO throw warning iff narargout < 3?
-    end
 end
